cbc71bb88aafc4bc7695b9b8d13b28a1f96fb777
dhairya6570 <126230153+dhairya6570@users.noreply.github.com>
2023-02-24 10:42:00 -0500

    Add files via upload

---
+++ day7.3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (){
+	
+	float a ;
+	float b ;
+	
+	printf("Temperature in Degree Celsius : ");
+	scanf("%f",&a);
+	
+	b = a*9/5 + 32  ;
+	
+	printf("Temperature in Degree Fahrenheit : %.1f",b);
+	
+	return 0;
+}
